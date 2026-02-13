4cf76aed74b197263fbd96b0bc759d724a86c096
Julian R <julian.rosero@outlook.es>
2026-02-12 19:09:33 -0800

    WIP : working on circle collision with square collision

--- Game/Private/Components/CircleColliderComponent.cpp
+++ Game/Private/Components/CircleColliderComponent.cpp
@@ -2,8 +2,6 @@
 #include "Game/Public/Components/TransformComponent.h"
 #include "Game/Public/Actor.h"
 
-
-
 CircleColliderComponent::CircleColliderComponent(std::weak_ptr<Actor> owner, float radius,
 												 exVector2 velocity,
 												 bool isStatic,
@@ -11,31 +9,39 @@ CircleColliderComponent::CircleColliderComponent(std::weak_ptr<Actor> owner, flo
 												 PhysicsComponent(owner, velocity, isStatic, isGravityEnabled),
 												 mRadius(radius)
 {
-
-};;;;;;;;;;;;;;;;;;;;;;
+	
+}
 
 bool CircleColliderComponent::IsCollisionDetected(std::weak_ptr<PhysicsComponent>& otherComponent) 
 {
 	if (!otherComponent.expired()) 
 	{
+		// Handle Collisions with Circle Collider Components
 		if (std::shared_ptr<CircleColliderComponent> OtherCircleColliderComponent = std::dynamic_pointer_cast<CircleColliderComponent>(otherComponent.lock())) 
 		{
+			// Owner Center Pos
 			exVector2 SelfCenterPos;
+			
+			// Other Circle Center Pos
 			exVector2 OtherCenterPos;
 
-			if (!mOwner.expired()) {
-				if (const std::shared_ptr<TransformComponent> TransformComp = mOwner.lock()->GetComponentOfType<TransformComponent>()) {
+			// Get Center Pos
+			if (!mOwner.expired()) 
+			{
+				if (const std::shared_ptr<TransformComponent> TransformComp = mOwner.lock()->GetComponentOfType<TransformComponent>()) 
+				{
 					SelfCenterPos = TransformComp->GetLocation();
 				}
 			}
 
+			// Get Other Circle Pos
 			if (!otherComponent.lock()->GetOwner().expired()) 
 			{
 				if (const std::shared_ptr<TransformComponent> TransformComp = OtherCircleColliderComponent->GetOwner().lock()->GetComponentOfType<TransformComponent>()) {
 					OtherCenterPos = TransformComp->GetLocation();
 				}
 			}
-
+			
 			exVector2 Length = SelfCenterPos - OtherCenterPos; 
 			float DistanceSquared = (Length.x * Length.x) + (Length.y * Length.y); // gets the distance between the circles
 			float RadiusSum = mRadius + OtherCircleColliderComponent->GetRadius();
@@ -44,6 +50,35 @@ bool CircleColliderComponent::IsCollisionDetected(std::weak_ptr<PhysicsComponent
 			return DistanceSquared < (RadiusSum * RadiusSum);
 		}
 
+		// Handle Collisions with Square Collider Components
+		if (std::shared_ptr<SquareColliderComponent> OtherSquareColliderComponent = std::dynamic_pointer_cast<SquareColliderComponent>(otherComponent.lock()))
+		{
+			// Owner Center Pos
+			exVector2 SelfCenterPos;
+			
+			// Square Center Pos
+			exVector2 OtherCenterPos;
+			
+			// Get Center Pos
+			if (!mOwner.expired()) 
+			{
+				if (const std::shared_ptr<TransformComponent> TransformComp = mOwner.lock()->GetComponentOfType<TransformComponent>()) 
+				{
+					SelfCenterPos = TransformComp->GetLocation();
+				}
+			}
+			
+			// Get Square Center Pos
+			if (!otherComponent.lock()->GetOwner().expired()) 
+			{
+				if (const std::shared_ptr<TransformComponent> TransformComp = OtherSquareColliderComponent->GetOwner().lock()->GetComponentOfType<TransformComponent>()) 
+					{
+					OtherCenterPos = TransformComp->GetLocation();
+				}
+			}
+			
+			
+		} 
 	}
 
 	// parent function
